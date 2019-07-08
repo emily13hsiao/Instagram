6289a85df0c967cb16cb353fa073bd47fc240ee3
Emily Hsiao <emily13hsiao@fb.com>
2019-07-08 15:28:15 -0700

    Initial Commit

---
+++ Instagram/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Instagram
+//
+//  Created by emily13hsiao on 7/8/19.
+//  Copyright © 2019 emily13hsiao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
---
+++ Instagram/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Instagram
+//
+//  Created by emily13hsiao on 7/8/19.
+//  Copyright © 2019 emily13hsiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
